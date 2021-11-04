667a65c019cb1f51a061b99449e2d3eddc975392
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-11-04 14:33:31 -0500

    made baseline for layout and cartPage components.

---
+++ src/pages/CartPage.js
@@ -0,0 +1,13 @@
+import { Box } from "@mui/system";
+import React from "react";
+import Layout from "../components/Layout/Layout";
+
+function CartPage() {
+  return (
+    <Layout>
+      <Box>There is the content of the CartPage</Box>
+    </Layout>
+  );
+}
+
+export default CartPage;
