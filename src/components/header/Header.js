7e5e94129137ac238fd0927de813d09edbe56f3a
dustnhgstrm <77700279+dustnhgstrm@users.noreply.github.com>
2021-11-04 14:55:20 -0500

    added a logo, and made some minor changes to various files.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,5 +1,7 @@
 import { Box } from "@mui/system";
 import React from "react";
+import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
+import { IconButton } from "@mui/material";
 
 function Header() {
   return (
