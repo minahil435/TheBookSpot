34c3749998f55a022167323c71010b40541f5381
Minahil Shafique <minahal.shafique@gmail.com>
2021-11-09 13:52:11 -0600

    cart icon showing items

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,8 +4,11 @@ import { AppBar, Toolbar, Typography } from "@mui/material";
 import ShoppingBagOutlinedIcon from "@mui/icons-material/ShoppingBagOutlined";
 import { IconButton } from "@mui/material";
 import { Link } from "react-router-dom";
+import { useShoppingCart } from "../../context/shoppingCartContext";
+
 
 function Header() {
+  const { shoppingCart } = useShoppingCart();
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar style={{ background: "black" }} position="static">
@@ -29,12 +32,18 @@ function Header() {
           >
             The Book Spot
           </Typography>
+         
           <Link to="/cart">
             <IconButton
-              style={{ background: "white" }}
-              sx={{ width: "50px", height: "50px" }}
-            >
-              <ShoppingBagOutlinedIcon fontSize="large" htmlColor="black" />
+                style={{ background: "white", position: 'relative' }}
+              sx={{ width: "50px", height: "50px" }}>
+              <ShoppingBagOutlinedIcon sx={{ color: (shoppingCart.length === 0) ? "black" : "red" }} fontSize="large" htmlColor="black"/>
+              <div style={{
+                position: 'absolute',
+                top: '19px',
+                fontSize: "large",
+                fontWeight:"bold"   
+              }}>{shoppingCart.length}</div>
             </IconButton>
           </Link>
         </Toolbar>
